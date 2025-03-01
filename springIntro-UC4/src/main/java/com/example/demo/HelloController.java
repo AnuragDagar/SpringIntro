3418abafe52d433f7555d250ae108847990edccc
Anuragdagar <anuragdagar8126@gmail.com>
2025-03-01 23:27:22 +0530

    UC4

---
+++ springIntro-UC4/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    // UC 1: GET Method - Simple Hello Message
+    // Test with: curl localhost:8080/hello -w "\n"
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from BridgeLabz!";
+    }
+
+    // UC 2: GET Method - Pass name as Query Parameter
+    // Test with: curl localhost:8080/hello/query?name=Mark -w "\n"
+    @GetMapping("/hello/query")
+    public String sayHelloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+
+    // UC 3: GET Method - Pass name as Path Variable
+    // Test with: curl localhost:8080/hello/param/Mark -w "\n"
+    @GetMapping("/hello/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+
+    // UC 4: POST Method - Pass firstName and lastName in the Body
+    // Test with: curl -X POST -H "Content-Type: application/json" -d '{"firstName":"Mark","lastName":"Taylor"}' "http://localhost:8080/hello/post" -w "\n"
+    @PostMapping("/hello/post")
+    public String sayHelloPost(@RequestBody UserDTO userDTO) {
+        return "Hello " + userDTO.getFirstName() + " " + userDTO.getLastName() + " from BridgeLabz!";
+    }
+
+
+}
